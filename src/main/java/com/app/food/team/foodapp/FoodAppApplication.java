34192556fdae76045ba310496fa8651e9cfa8138
victor diaz <vdmpeniel@yahoo.es>
2022-11-08 17:13:57 -0500

    first commit

---
+++ src/main/java/com/app/food/team/foodapp/FoodAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.food.team.foodapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/food/team/foodapp/FoodAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.food.team.foodapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
