95f36408407cf5039d07518229e83634a12bb5b5
firetending <102393310+firetending@users.noreply.github.com>
2022-12-09 01:44:05 -0500

    endpoints for updateMenu and getMenuById; readme updates; auth still .permitAll()

--- src/main/java/com/app/food/team/foodapp/controller/MenuController.java
+++ src/main/java/com/app/food/team/foodapp/controller/MenuController.java
@@ -1,13 +1,16 @@
 package com.app.food.team.foodapp.controller;
 
 import com.app.food.team.foodapp.dto.MenuConverterDto;
-import com.app.food.team.foodapp.dto.RecipeDto;
 import com.app.food.team.foodapp.dto.ResponseDto;
 import com.app.food.team.foodapp.model.Item;
 import com.app.food.team.foodapp.model.Menu;
 import com.app.food.team.foodapp.model.RecipeApiWrapper;
 import com.app.food.team.foodapp.repository.ItemRepository;
 import com.app.food.team.foodapp.repository.MenuRepository;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +18,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.OK;
@@ -37,18 +40,11 @@ public class MenuController {
 
     @PostMapping(path = "menu/new", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity createNewMenu(@RequestBody List<RecipeApiWrapper> data) {
-        log.info("[ItemController] ----> /data/menu");
+        log.info("[MenuController] ----> /data/menu/new");
 
         // MenuConvertDto unwraps the recipe objects and saves a set of them in a menu
         MenuConverterDto menuData = new MenuConverterDto(data);
-        List<Item> menuDataItems = menuData.getMenuItems();
-        //check if recipes exist in database; fill in existing recipes where possible
-        for (int i = 0; i<menuDataItems.size(); i++) {
-            String checkExternalId = menuDataItems.get(i).getExternalId();
-            if (itemRepo.existsByExternalId(checkExternalId)) {
-                menuDataItems.set(i,itemRepo.findByExternalId(checkExternalId));
-            }
-        }
+        checkForPersistedMenuItems(menuData);
         Menu newMenu = menuData.getMenu();
 
         //declarations for ResponseDto builder variables
@@ -71,10 +67,69 @@ public class MenuController {
         );
     }
 
+    @PostMapping(path = "menu/update", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity updateMenu(@RequestParam int id,
+                                     @RequestBody List<RecipeApiWrapper> data) {
+        log.info("[MenuController] ----> /data/menu/update");
+
+        //declarations for ResponseDto builder variables
+        String message = "";
+
+        //check for existing menu by id
+        Optional<Menu> menuCheck = menuRepo.findById(id);
+        Menu existingMenu = new Menu();
+
+        //if present, unwrap new data and put it in existing menu
+        if (menuCheck.isPresent()) {
+            existingMenu = menuCheck.get();
+
+            // MenuConvertDto unwraps the recipe objects and saves a set of them in a menu
+            MenuConverterDto menuData = new MenuConverterDto(data);
+            checkForPersistedMenuItems(menuData);
+            existingMenu.setMenuItems(menuData.getMenuItems());
+
+            //save menu
+            menuRepo.save(existingMenu);
+            message = "Menu updated";
+        } else {
+            message = "No menu with that id was found";
+        }
+
+        return ResponseEntity.ok(
+                ResponseDto.builder().timeStamp(now()).status(OK).statusCode(OK.value())
+                        .message(message)
+                        .data(new HashMap<>(){{
+                            put("internal id", id);
+                        }})
+                        .build()
+        );
+    }
+
     @GetMapping(path = "menu/all", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Menu> getAllMenus() {
         return menuRepo.findAll();
     }
+
+//    @GetMapping(path = "menu/all", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public String getAllMenuMetadata() {
+//        menuRepo.findAll()
+//        return "";
+//    }
+
+    @GetMapping(path = "menu", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getMenuById(@RequestParam int id) {
+        Optional<Menu> optExistingMenu = menuRepo.findById(id);
+        String responseJson = "";
+        if (optExistingMenu.isPresent()) {
+            responseJson = new Gson().toJson(new MenuConverterDto(optExistingMenu.get()));
+        } else {
+            responseJson = "No menu with that id was found";
+        }
+
+        return responseJson;
+    }
+
+
 
     // example arguments
     // @RequestHeader(value="User-Agent") String userAgent
@@ -82,13 +137,28 @@ public class MenuController {
     // returns recipe close to edamam format, some nested data displayed differently like tags for diet labels, etc
     @GetMapping(path = "menu/items", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<RecipeApiWrapper> getItemByInternalId(@RequestParam(value = "internalId") int internalId){
-        log.info("[ItemController] ----> data/item");
+        log.info("[MenuController] ----> data/item");
 
         Optional<Menu> optionalMenu = menuRepo.findById(internalId);
         Menu menu = new Menu();
         if (optionalMenu.isPresent()) {
             menu = optionalMenu.get();
         }
-        return new MenuConverterDto(menu).getWrappedMenuItems();
+        return new MenuConverterDto(menu).getWrappedMenuItemsForJSON();
+    }
+
+
+    //utility methods
+    public List<Item> checkForPersistedMenuItems(MenuConverterDto menuData) {
+
+        //check if recipes exist in database; fill in existing recipes where possible
+        List<Item> menuDataItems = menuData.getMenuItems();
+        for (int i = 0; i<menuDataItems.size(); i++) {
+            String checkExternalId = menuDataItems.get(i).getExternalId();
+            if (itemRepo.existsByExternalId(checkExternalId)) {
+                menuDataItems.set(i,itemRepo.findByExternalId(checkExternalId));
+            }
+        }
+        return menuDataItems;
     }
 }
--- src/main/java/com/app/food/team/foodapp/dto/MenuConverterDto.java
+++ src/main/java/com/app/food/team/foodapp/dto/MenuConverterDto.java
@@ -2,13 +2,12 @@ package com.app.food.team.foodapp.dto;
 
 import com.app.food.team.foodapp.model.*;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
-import lombok.experimental.SuperBuilder;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
@@ -16,14 +15,17 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 //@SuperBuilder // https://en.wikipedia.org/wiki/Builder_pattern#Java
 @JsonInclude(NON_NULL)  // https://www.javaguides.net/2019/04/jackson-jsoninclude-example.html
 public class MenuConverterDto {
-
-    protected List<RecipeApiWrapper> wrappedMenuItems;
+    @Expose //field to be serialized by Gson
+    protected int internalId;
+    @SerializedName("menuItemList")
+    @Expose
+    protected List<RecipeApiWrapper> wrappedMenuItemsForJSON = new ArrayList<>();
     protected List<Item> menuItems = new ArrayList<>();
     protected Menu menu;
 
     public MenuConverterDto(List<RecipeApiWrapper> data) {
-        this.wrappedMenuItems = data;
-        for (RecipeApiWrapper wrappedRecipe : wrappedMenuItems) {
+        this.wrappedMenuItemsForJSON = data;
+        for (RecipeApiWrapper wrappedRecipe : wrappedMenuItemsForJSON) {
                 RecipeDto recipeDto = new RecipeDto(wrappedRecipe.getRecipe());
                 menuItems.add(recipeDto.getRecipe());
         }
@@ -33,7 +35,8 @@ public class MenuConverterDto {
     public MenuConverterDto(Menu menuObject) {
         for (Item recipe : menuObject.getMenuItems()) {
             RecipeApiWrapper wrappedRecipe = new RecipeApiWrapper(recipe);
-            wrappedMenuItems.add(wrappedRecipe);
+            this.wrappedMenuItemsForJSON.add(wrappedRecipe);
+            this.internalId = menuObject.getInternalId();
         }
     }
 }
